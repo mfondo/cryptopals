8bf8dde427980c357ceca30909d619e2da86d0e1
Mike Friesen <m.fondo@gmail.com>
2014-09-10 20:56:40 -0500

    solved challenge 1-3

--- Challenge_1_3.cpp
+++ Challenge_1_3.cpp
@@ -3,7 +3,14 @@
 #include <cfloat>
 #include "Hex.h"
 #include "Xor.h"
+#include <map>
 #include "CharFrequency.h"
+
+struct FloatCompare {
+	bool operator()(const float& a, const float& b) const {
+		return a < b;
+	}
+};
 
 int main(int argc, char** argv) {
 	string encrypted = string("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
@@ -13,25 +20,25 @@ int main(int argc, char** argv) {
 	
 	char *decryptedBytes = (char *)malloc(encryptedBytesLength);
 	string *decryptedStr;
-	float minFrequencyDistance = FLT_MAX;
-	string *minFrequencyDistanceStr = NULL;
 	float frequencyDistance;
+	const int maxMapSize = 10;//adjust as necessary if top maxMapSize results does not contain correct decrypted string
+	std::multimap<float, string*, FloatCompare> orderedFrequencyDistanceMap;
+	std::multimap<float, string*>::iterator iter;
 	for(char c = CHAR_MIN; c < CHAR_MAX; c++) {	
 		Xor::xorBytes(encryptedBytes, c, decryptedBytes, encryptedBytesLength);
 		decryptedStr = new string(decryptedBytes, encryptedBytesLength);
 		frequencyDistance = CharFrequency::calculateDistanceFromCommonFrequency(decryptedStr);
-		cout << "Distance:" << frequencyDistance << "str:" << *decryptedStr << endl;
-		if(frequencyDistance < minFrequencyDistance) {
-			if(minFrequencyDistanceStr != NULL) {
-				delete minFrequencyDistanceStr;
-			}
-			minFrequencyDistanceStr = decryptedStr;
-			minFrequencyDistance = frequencyDistance;
-		} else {
-			delete decryptedStr;
-		}		
+		orderedFrequencyDistanceMap.insert(std::pair<float, string*>(frequencyDistance, decryptedStr));
+		if(orderedFrequencyDistanceMap.size() > maxMapSize) {
+			iter = orderedFrequencyDistanceMap.end();
+			iter--;
+			delete (*iter).second;
+			orderedFrequencyDistanceMap.erase(iter);
+		}
 	}
-	cout << "Min Frequency Distance:" << minFrequencyDistance << ":" << *minFrequencyDistanceStr << endl;
+	for(iter = orderedFrequencyDistanceMap.begin(); iter != orderedFrequencyDistanceMap.end(); iter++) {
+		cout << "key:" << (*iter).first << ":val:" << *((*iter).second) << endl;
+	}
 	free(decryptedBytes);	
-	cout << "Challenge 1-3 Success" << endl;//TODO wrong
+	cout << "Challenge 1-3 Success" << endl;
 }
